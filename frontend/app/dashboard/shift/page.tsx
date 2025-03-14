b5290ae755c60fc20ee6dbf79ca4b54f430a2da0
Efa <efatariku07@gamil.com>
2025-03-14 21:58:05 +0300

    typescript error fixed

--- frontend/app/dashboard/shift/page.tsx
+++ frontend/app/dashboard/shift/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useState, useEffect, JSXElementConstructor, ReactElement, ReactNode, ReactPortal } from "react";
+import { useState, useEffect } from "react";
 import { Card, CardContent } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
